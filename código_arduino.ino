470355bba818c62c94a0d44cc0b8f03a74e443cd
Paola Hernández <70167540+PaoAlonzo@users.noreply.github.com>
2024-04-19 12:05:54 -0600

    Update código_arduino.ino

--- código_arduino.ino
+++ código_arduino.ino
@@ -4,3 +4,14 @@ void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
+void loop() {
+  if (Serial.available() > 0) {
+    char data = Serial.read();
+    if (data == '1') {
+      digitalWrite(ledPin, HIGH);
+    }
+    else if (data == '0') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
